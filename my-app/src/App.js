c9d650e47c12040398f7a4d1f27339744e05dccc
ronitbatra <rbatra210@gmail.com>
2025-04-22 12:16:22 -0400

    basic react app

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hello from './components/Hello';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
